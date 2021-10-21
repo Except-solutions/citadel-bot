de7dabd7887987470c22f945fbb46319d11912b6
goddesseyes <goddesseyes@protonmail.com>
2021-10-22 01:49:51 +0300

    fix: CI

--- server/modules/scheduling/dobloe_utlo.py
+++ server/modules/scheduling/dobloe_utlo.py
@@ -1,3 +1,5 @@
+from asyncio import sleep
+
 from server.app import dp, scheduler
 from server.settings.dev import settings
 
@@ -5,3 +7,10 @@ from server.settings.dev import settings
 @scheduler.scheduled_job('cron', day_of_week='mon-fri', hour=7, minute=0)
 async def send_dobloe_utlo():
     await dp.bot.send_photo(chat_id=settings.group_id, photo=settings.dobloe_utlo_pic)
+
+
+@scheduler.scheduled_job('cron', day_of_week='mon-fri', hour=15, minute=0)
+async def send_pokinut_zavod():
+    await dp.bot.send_message(text='Внимание всем покинуть территорию завода!', chat_id=settings.group_id)
+    await sleep(2)
+    await dp.bot.send_message(text='Повторяю всем покинуть территорию завода!', chat_id=settings.group_id)
